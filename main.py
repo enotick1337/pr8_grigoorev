6af3f7fafbfabb2d549526a0b6075bcb6619060e
enotick1337 <101464958+enotick1337@users.noreply.github.com>
2022-12-07 20:35:50 +0300

    Изменили main.py на ветке dev

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import string
 def func(parol):
     st1 = string.ascii_lowercase
     st2 = string.ascii_uppercase
+    st3 = string.digits
     for i in range(11):
-            parol += parol.join(random.choices(st1+st2))
+            parol += parol.join(random.choices(st1+st2+st3))
     return parol
 parol = ''
 print(func(parol))
