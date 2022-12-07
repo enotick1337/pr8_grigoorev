b73a54031a17deacfca44c16ea1000aa5a3d7cbf
enotick1337 <101464958+enotick1337@users.noreply.github.com>
2022-12-07 20:26:34 +0300

    Добавили в репозиторию main.py на ветке hotfix

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import random
 import string
 def func(parol):
     st1 = string.ascii_lowercase
+    st2 = string.ascii_uppercase
     for i in range(11):
-            parol += parol.join(random.choices(st1))
+            parol += parol.join(random.choices(st1+st2))
     return parol
 parol = ''
 print(func(parol))
